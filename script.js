05f569cb60c09e794658623c2da70fd912b29475
SrHerbe <60781159+SrHerbe@users.noreply.github.com>
2023-03-03 10:02:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+const csvFileInput = document.getElementById("fileInput");
+const downloadBtn = document.getElementById("downloadBtn");
+let csvData;
+
+csvFileInput.addEventListener("change", (e) => {
+    const file = csvFileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        const data = e.target.result;
+
+        //1. Quitar filas dónde la columna VALOR PREFIJADO es menor que 4 y la columna STATUS SISTEMA es "IMPR INBO LIB."
+
+        let filtro1 = data.split("\n");
+
+        filtro1.forEach((v, i) => {
+            filtro1[i] = v.split(";");
+        });
+
+        filtro1 = filtro1.filter((v) => {
+            return parseFloat(v[5]) >= 3 && v[6] != "IMPR INBO LIB.";
+        });
+
+        //2. Creamos el array final con la orden, material y texto breve material
+
+        const uniqueValues = new Set();
+
+        filtro1.forEach((arrFinal) => uniqueValues.add(arrFinal[0]));
+
+        // ArrFinal = Array.from(uniqueValues, value => [value]);
+        let ArrFinal = Array.from(uniqueValues, (value) => {
+            const matchingArray = filtro1.find(
+                (arrFinal) => arrFinal[0] === value
+            );
+            if (matchingArray) {
+                return [value, matchingArray[1], matchingArray[2]];
+            } else {
+                return [value];
+            }
+        });
+
+        //3. Añadimos
+        // T= TERMINADA
+        // A= ACTIVA
+        // P= PENDIENTE
+
+        ArrFinal.forEach((v, i) => {
+            let cont = 0;
+            filtro1.forEach((v2) => {
+                if (v[0] == v2[0]) {
+                    ArrFinal[i].push(v2[4]);
+                    let estado;
+                    if (
+                        v2[9] == 1 &&
+                        v2[15] == 1 &&
+                        parseFloat(v2[12]) >= 0.7 * parseFloat(v2[5])
+                    ) {
+                        ArrFinal[i].push("T");
+                    } else if (cont == 0) {
+                        ArrFinal[i].push("A");
+                        cont++;
+                    } else {
+                        ArrFinal[i].push("P");
+                    }
+                }
+            });
+        });
+
+        if (ArrFinal[ArrFinal.length - 1][0] === "") {
+            ArrFinal.pop();
+        }
+
+        //Conversión a string
+
+        ArrFinal.forEach((v, i) => {
+            ArrFinal[i] = ArrFinal[i].join(";");
+        });
+        csvData = ArrFinal.join("\n");
+
+        // console.log(ArrFinal);
+
+        //Descargar csv
+    };
+
+    reader.readAsText(file);
+});
+
+
+
+downloadBtn.addEventListener("click", () => {
+    const blob = new Blob([csvData], { type: "text/csv" });
+
+    // Crea un objeto URL con el objeto Blob creado
+    const url = URL.createObjectURL(blob);
+
+    // Crea un elemento <a> con el atributo href configurado con el objeto URL creado
+    const link = document.createElement("a");
+    link.href = url;
+
+    // Configura el atributo download del elemento <a> con el nombre del archivo que deseas descargar
+    link.download = "resultados.csv";
+
+    // Dispara un evento click en el elemento <a> para iniciar la descarga del archivo
+    link.click();
+});
